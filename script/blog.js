fe8f906f11135b8c69b7d15182bcd39330130737
christian-ahanonu <ahanonuchristian116@gmail.com>
2025-02-20 13:55:09 +0100

    changed the image path to relative file path

--- script/blog.js
+++ script/blog.js
@@ -7,55 +7,55 @@ const blogPostData = [
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/fruit-basket.webp",
+        image: "images/blog/fruit-basket.webp",
         title: "Fruit Basket",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/green-capsicum.png",
+        image: "images/blog/green-capsicum.png",
         title: "Green Capsicum",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/cow.webp",
+        image: "images/blog/cow.webp",
         title: "Cow",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/sliced-cucumber.webp",
+        image: "images/blog/sliced-cucumber.webp",
         title: "Sliced Cucumber",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/stacked-oranges.webp",
+        image: "images/blog/stacked-oranges.webp",
         title: "Stacked Oranges",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/tomatos-greens.webp",
+        image: "images/blog/tomatos-greens.webp",
         title: "Tomatos Greens",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/halloween.png",
+        image: "images/blog/halloween.png",
         title: "Halloween",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"      
     },
     {
-        image: "/images/blog/coffee-bean.webp",
+        image: "images/blog/coffee-bean.webp",
         title: "Coffee Bean",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
     },
     {
-        image: "/images/blog/tomatos-on-farm.webp",
+        image: "images/blog/tomatos-on-farm.webp",
         title: "Tomatos On Farm",
         headline: "Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.",
         link: "Read More ➡"
